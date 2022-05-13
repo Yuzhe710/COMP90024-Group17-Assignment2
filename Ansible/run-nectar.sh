04183fa8e0af4c5d2a607c5c8b39dbab78df5316
yeager20001118 <yeager.tian@gmail.com>
2022-05-14 05:31:39 +0800

    fix

--- Ansible/run-nectar.sh
+++ Ansible/run-nectar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-. ./openrc.sh; ansible-playbook -i hosts -u ubuntu --key-file=~/.ssh/ccc-group17.pem nectar.yaml
+. ./openrc.sh; ansible-playbook -i ./inventory/hosts -u ubuntu --key-file=~/.ssh/ccc-group17.pem nectar.yaml
 
